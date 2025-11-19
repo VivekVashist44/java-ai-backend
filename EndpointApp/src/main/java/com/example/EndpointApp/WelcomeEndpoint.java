1256e91d709598029c87a061b573009a1461a894
vivekvashist44 <vivekvashist4444@gmail.com>
2025-11-19 15:48:27 +0530

    Adding a new endpoint which accept query parameter

--- EndpointApp/src/main/java/com/example/EndpointApp/WelcomeEndpoint.java
+++ EndpointApp/src/main/java/com/example/EndpointApp/WelcomeEndpoint.java
@@ -2,11 +2,16 @@ package com.example.EndpointApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class WelcomeEndpoint{
     @GetMapping("/welcome")
     public String welcome(){
         return "Hello, AI Backend!";
+    }
+    @GetMapping("/greet")
+    public String greet(@RequestParam String name){
+        return "Hello ,"+name+"! Welcome to AI Backend";
     }
 }
